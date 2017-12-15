dc9b933c75749f99f89888c96c302b40699859b6
Duc Anh Nguyen <ndaths@gmail.com>
2017-12-15 10:22:32 +0700

    add google map

--- src/containers/dashboard.jsx
+++ src/containers/dashboard.jsx
@@ -1,26 +1,90 @@
 import React, {Component} from 'react';
 import Tabs from '../components/Tabs.jsx';
 import Tab from '../components/Tab.jsx';
-import GMap from '../components/GMap.jsx';
+import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
 
-const Dashboard = () => (
-	<div className="dashboard-container">
-		<div className="topbar">
-			<span>Blood Donor Finder</span>
-		</div>
-		<div className="left-panel">
-			<Tabs defaultActiveTabIndex={0}>
-				<Tab tabName={'Filter'} linkClassName={'link-class-0'}>
-					<p>content 0</p>
-				</Tab>
-				<Tab tabName={'Information'} linkClassName={'link-class-1'}>
-					
-				</Tab>
-			</Tabs>
-		</div>
-		<div className="right-panel">
-			<GMap></GMap>	
-		</div>
-	</div>
-)
+export class Dashboard extends Component {
+
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			coords: {
+			  lat: 51.5258541,
+			  lng: -0.08040660000006028
+			},
+			mapKey: {
+				v: '3.exp',
+				key: 'YOUR_API_KEY'
+			}
+		}
+	}
+
+	onMapCreated(map) {
+		map.setOptions({
+			disableDefaultUI: true
+		});
+	}
+
+	onDragEnd(e) {
+		console.log('onDragEnd', e);
+	}
+
+	onCloseClick() {
+		console.log('onCloseClick');
+	}
+
+	onClick(e) {
+		console.log('onClick', e);
+	}
+
+	render() {
+		return (
+			<div className="dashboard-container">
+				<div className="topbar">
+					<span>Blood Donor Finder</span>
+				</div>
+				<div className="left-panel">
+					<Tabs defaultActiveTabIndex={0}>
+						<Tab tabName={'Filter'} linkClassName={'link-class-0'}>
+							<p>content 0</p>
+						</Tab>
+						<Tab tabName={'Information'} linkClassName={'link-class-1'}>
+							
+						</Tab>
+					</Tabs>
+				</div>
+				<div className="right-panel">
+					<Gmaps
+						className="gmap-container"
+						width={'100%'}
+						height={'100%'}
+						lat={this.state.coords.lat}
+						lng={this.state.coords.lng}
+						zoom={12}
+						loadingMessage={'Be happy'}
+						params={this.state.params}
+						onMapCreated={this.onMapCreated}>
+
+						<Marker
+							lat={this.state.coords.lat}
+							lng={this.state.coords.lng}
+							draggable={true}
+							onDragEnd={this.onDragEnd} />
+						<InfoWindow
+							lat={this.state.coords.lat}
+							lng={this.state.coords.lng}
+							content={'Hello, React :)'}
+							onCloseClick={this.onCloseClick} />
+						<Circle
+							lat={this.state.coords.lat}
+							lng={this.state.coords.lng}
+							radius={500}
+							onClick={this.onClick} />
+					</Gmaps>
+				</div>
+			</div>
+		)
+	}
+};
+
 export default Dashboard;
